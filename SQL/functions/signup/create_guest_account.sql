9291944159c8d563cc3137a2b2c15361938d85cf
YuhanZhang11 <83880227+YuhanZhang11@users.noreply.github.com>
2023-02-22 17:08:52 -0500

    Update create_guest_account.sql

--- SQL/functions/signup/create_guest_account.sql
+++ SQL/functions/signup/create_guest_account.sql
@@ -3,6 +3,8 @@
 -- Function: insert a new record into People table and Guest table with the input guest info, where the new guest account is not activated
 -- Input: new guest's information: username, password, and email address
 -- Output: new guest's id
+
+DELIMITER //
 CREATE PROCEDURE create_guest_account (
   IN username       VARCHAR(100),
   IN password       VARCHAR(100),
@@ -10,7 +12,7 @@ CREATE PROCEDURE create_guest_account (
   OUT user_ID       BIGINT
 )
 BEGIN
-  SET @default_credit_level = 5
+  SET @default_credit_level = 5;
   SET @new_user_id = (
     SELECT MAX(People.ID) + 1
     FROM People
@@ -19,5 +21,6 @@ BEGIN
     VALUE(@new_user_id, username, password, email_address, TRUE);
   INSERT INTO Guest
     VALUE(@new_user_id, FALSE, @default_credit_level, @default_credit_level, DEFAULT);
-  SELECT @new_user_id INTO user_ID
-END
+  SELECT @new_user_id INTO user_ID;
+END //
+DELIMITER ;

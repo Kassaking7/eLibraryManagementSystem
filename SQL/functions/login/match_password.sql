82c47cb371bdc320ffd56067f33ab066314ad4e3
Zihan Pan <zihanpan8378@gmail.com>
2023-02-21 22:04:20 -0500

    Update

--- SQL/functions/login/match_password.sql
+++ SQL/functions/login/match_password.sql
@@ -1,26 +1,23 @@
+DELIMITER //
+
 -- Caller: guests or administrators
 -- Senario: a guest or an administrator wants to login to the system
 -- Function: Check if the input user id exists and the input password matches with the id, and distinguish if the user is a guest or an admin
 -- Input: user_id, password
 -- Output: whether the id and password matches, and whether the user is a guest
 CREATE PROCEDURE match_password (
-  IN user_id    BIGINT, 
-  IN password   VARCHAR(100),
-  OUT match     BOOLEAN,
-  OUT is_guest  BOOLEAN
+  IN user_id          BIGINT, 
+  IN password         VARCHAR(100),
+  OUT password_match  BOOLEAN
 )
-  BEGIN
+BEGIN
     SELECT CASE 
-      WHEN COUNT(*) = 1 THEN CASE
-        WHEN People.password = password THEN TRUE
-        ELSE FALSE
-      END
+      WHEN COUNT(*) >= 1 THEN TRUE
       ELSE FALSE 
-    END INTO match,
-    CASE
-      WHEN COUNT(*) = 1 THEN People.is_guest
-      ELSE FALSE 
-    END INTO is_guest
+    END INTO password_match
     FROM People
-    WHERE People.ID = user_id;
-  END
+    WHERE People.ID = user_id
+    AND People.password = password;
+END //
+
+DELIMITER ;
--- SQL/triggers/add_copy.sql
+++ SQL/triggers/add_copy.sql
@@ -1,9 +1,7 @@
 CREATE TRIGGER auto_increment_copy_id BEFORE INSERT ON Copy
-FOR EACH ROW 
-BEGIN
-    SET NEW.copy_ID = (
-       SELECT IFNULL(MAX(Copy.copy_ID), 0) + 1
-       FROM Copy
-       WHERE Copy.ISBN = NEW.ISBN
-    );
-END
+FOR EACH ROW
+SET NEW.copy_ID = (
+  SELECT IFNULL(MAX(Copy.copy_ID), 0) + 1
+  FROM Copy
+  WHERE Copy.ISBN = NEW.ISBN
+)

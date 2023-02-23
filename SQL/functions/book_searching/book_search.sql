5b12c6455c205a3748c28b0d4063f6f8a72e7af0
YuhanZhang11 <83880227+YuhanZhang11@users.noreply.github.com>
2023-02-23 16:58:33 -0500

    Update book_search.sql

--- SQL/functions/book_searching/book_search.sql
+++ SQL/functions/book_searching/book_search.sql
@@ -4,34 +4,27 @@
 -- Input: ISBN, title, number of pages, tag name, author name, author nationality, author birth year, publisher name, publication year
 -- Output: ISBN of the search books
 
+
 DELIMITER //
 CREATE PROCEDURE book_search (
   IN ISBN               VARCHAR(20),
   IN title              VARCHAR(50),
-  IN number_of_pages    INT,
   IN tag_name           VARCHAR(50),
   IN author_name        VARCHAR(100),
-  IN author_nationality VARCHAR(100),
-  IN author_birth_year  VARCHAR(7),
-  IN publisher_name     VARCHAR(100),
-  IN publication_year   VARCHAR(7),
-  OUT result_ISBN       VARCHAR(20)
+  IN publisher_name     VARCHAR(100)
 )
   BEGIN
-    SELECT DISTINCT Book.ISBN INTO result_ISBN
+    SELECT DISTINCT Book.ISBN
     FROM Book
     INNER JOIN Has_tag ON (Book.ISBN = Has_tag.ISBN)
     INNER JOIN Tag ON (Has_tag.tag_name = Tag.name)
     INNER JOIN Written_by ON (Book.ISBN = Written_by.ISBN)
     INNER JOIN Author ON (Written_by.author_ID = Author.ID)
     INNER JOIN Publisher ON (Book.publisher_ID = Publisher.ID)
-    WHERE Book.title LIKE CONCAT(IFNULL(title, ""), "%")
-    AND Book.number_of_pages LIKE CONCAT(IFNULL(number_of_pages, ""), "%")
-    AND Tag.name LIKE CONCAT(IFNULL(tag_name, ""), "%")
-    AND Author.name LIKE CONCAT(IFNULL(author_name, ""), "%")
-    AND Author.nationality LIKE CONCAT(IFNULL(author_nationality, ""), "%")
-    AND Author.birth_year LIKE CONCAT(IFNULL(author_birth_year, ""), "%")
-    AND Publisher.name LIKE CONCAT(IFNULL(publisher_name, ""), "%")
-    AND Book.publication_year LIKE CONCAT(IFNULL(publication_year, ""), "%");
+    WHERE (isnull(ISBN) or ISBN = Book.ISBN)
+	AND Book.title LIKE CONCAT(IFNULL(title, ""), "%")
+	AND Tag.name LIKE CONCAT(IFNULL(tag_name, ""), "%")
+	AND Author.name LIKE CONCAT(IFNULL(author_name, ""), "%")
+	AND Publisher.name LIKE CONCAT(IFNULL(publisher_name, ""), "%");
   END //
   DELIMITER ;

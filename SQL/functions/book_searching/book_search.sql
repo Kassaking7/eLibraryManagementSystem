d571a3695760cb29930b7d999880ca1139ea93a4
YuhanZhang11 <83880227+YuhanZhang11@users.noreply.github.com>
2023-02-22 19:40:51 -0500

    Update book_search.sql

--- SQL/functions/book_searching/book_search.sql
+++ SQL/functions/book_searching/book_search.sql
@@ -25,13 +25,13 @@ CREATE PROCEDURE book_search (
     INNER JOIN Written_by ON (Book.ISBN = Written_by.ISBN)
     INNER JOIN Author ON (Written_by.author_ID = Author.ID)
     INNER JOIN Publisher ON (Book.publisher_ID = Publisher.ID)
-    WHERE Book.title LIKE CONCAT(ISNULL(title, ""), "%")
-    AND Book.number_of_pages LIKE CONCAT(ISNULL(number_of_pages, ""), "%")
-    AND Tag.name LIKE CONCAT(ISNULL(tag_name, ""), "%")
-    AND Author.name LIKE CONCAT(ISNULL(author_name, ""), "%")
-    AND Author.nationality LIKE CONCAT(ISNULL(author_nationality, ""), "%")
-    AND Author.birth_year LIKE CONCAT(ISNULL(author_birth_year, ""), "%")
-    AND Publisher.name LIKE CONCAT(ISNULL(publisher_name, ""), "%")
-    AND Book.publication_year LIKE CONCAT(ISNULL(publication_year, ""), "%");
+    WHERE Book.title LIKE CONCAT(IFNULL(title, ""), "%")
+    AND Book.number_of_pages LIKE CONCAT(IFNULL(number_of_pages, ""), "%")
+    AND Tag.name LIKE CONCAT(IFNULL(tag_name, ""), "%")
+    AND Author.name LIKE CONCAT(IFNULL(author_name, ""), "%")
+    AND Author.nationality LIKE CONCAT(IFNULL(author_nationality, ""), "%")
+    AND Author.birth_year LIKE CONCAT(IFNULL(author_birth_year, ""), "%")
+    AND Publisher.name LIKE CONCAT(IFNULL(publisher_name, ""), "%")
+    AND Book.publication_year LIKE CONCAT(IFNULL(publication_year, ""), "%");
   END //
   DELIMITER ;

44f353d3f8dcef3081c41b83e2995f8f87d95778
Zihan Pan <zihanpan8378@gmail.com>
2023-02-25 14:51:04 -0500

    Update

--- SQL/triggers/insert_borrowing.sql
+++ SQL/triggers/insert_borrowing.sql
@@ -6,8 +6,8 @@ BEGIN
   SET NEW.borrowing_ID = (
     SELECT IFNULL(MAX(Borrowing.borrowing_ID), 0) + 1
     FROM Borrowing
-    WHERE Borrowing.ISBN = Borrowing.ISBN
-    AND Borrowing.copy_ID = Borrowing.copy_ID
+    WHERE Borrowing.ISBN = NEW.ISBN
+    AND Borrowing.copy_ID = NEW.copy_ID
   );
   SET NEW.borrow_date = CONVERT(DATE, CURDATE());
   SET NEW.return_deadline = CONVERT(DATE, ADDDATE(CURDATE(),15));

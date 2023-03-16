92ab96a17faac3f8f6fb8183d020da185b736ff8
Zetian Li - ikun <68205293+fatday@users.noreply.github.com>
2023-03-15 20:03:17 -0400

    Update cancel_register.sql

--- SQL/functions/Event_hosting/cancel_register.sql
+++ SQL/functions/Event_hosting/cancel_register.sql
@@ -1,1 +1,46 @@
+-- Caller: admin or guests
+-- Senario: cancel the registration of an event
+-- Input: guest_ID, event_id
+-- Output: if_succeeded, if_event_exist
 
+DELIMITER //
+CREATE PROCEDURE register_event(
+    IN guest_ID            BIGINT, 
+    IN event_ID            BIGINT,
+    OUT if_succeeded       BOOLEAN,
+    OUT if_event_exist     BOOLEAN
+)
+
+proc_label: BEGIN
+    -- to check if the event exist
+    SET if_event_exist = (event_ID in (SELECT ID FROM Event));
+    SET if_succeeded = FALSE;
+
+    -- if cannot register, then exist the procedure
+    IF (if_event_exist = FALSE) THEN
+        LEAVE proc_label;
+    END IF;
+
+    -- check if event ended
+    SET if_succeeded = 
+    CASE
+        WHEN (SELECT end_date_time FROM Event WHERE Event.ID = event_ID) < NOW() THEN FALSE
+        ELSE TRUE
+    END;
+
+    -- if cannot cancel, then exist the procedure
+    IF (if_succeeded = FALSE) THEN
+        LEAVE proc_label;
+    END IF;
+
+    -- decrease the current amount of participant in the event
+    UPDATE Event
+    SET Event.current_amount = Event.current_amount - 1
+    WHERE Event.ID = event_ID;
+
+    -- remove the record from Registration table
+    DELETE FROM Registration
+    WHERE (Registration.guest_ID, Registration.event_ID) = (guest_ID, event_ID);
+
+END //
+DELIMITER;

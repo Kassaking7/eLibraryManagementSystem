637f62675dce84a04b85f9d0b2cdbe08e8614a88
YuhanZhang11 <83880227+YuhanZhang11@users.noreply.github.com>
2023-03-18 19:11:47 -0400

    Update test.sql

--- SQL/functions/Tests/Production/test.sql
+++ SQL/functions/Tests/Production/test.sql
@@ -29,8 +29,43 @@ call activate_guest_account(110);
 
 
 ##############################
-## test_activateGuestAccount
+## Event_SetUp
 ##############################
+# Event creation failed
+-- Admin cannot host event
+call event_setup('event1', '2023-05-01 13:00:00', '2023-05-01 14:00:00', 5, 'Event for kids', 1, 1, @t1);
+select @t1; #Expect: 0
+
+-- Ouside of the closing hour
+call event_setup('event11', '2023-05-11 13:00:00', '2023-05-11 7:00:00', 5, 'Event for kids', 3, 1, @t3);
+select @t3; #Expect: 0
+call event_setup('event21', '2023-05-21 13:00:00', '2023-05-21 20:00:00', 5, 'Event for kids', 3, 1, @t4);
+select @t4; #Expect: 0
+
+-- Overlap Time
+call event_setup('event31', '2023-05-01 13:00:00', '2023-05-01 14:00:00', 5, 'Event1', 1, 1, @t5);
+select @t5; #Expect: 0
+call event_setup('event1', '2023-05-01 14:00:00', '2023-05-01 15:00:00', 5, 'Event2', 1, 1, @t6);
+select @t6; #Expect: 0
+
+
+# Event creation succeed
+-- First event inserted
+call event_setup('event1', '2023-05-01 13:00:00', '2023-05-01 14:00:00', 5, 'Reading party', 3, 2, @t2);
+select @t2; #Expect: 1
+
+-- A event right after a event
+call event_setup('event2', '2023-05-01 14:00:01', '2023-05-01 15:00:00', 5, 'Reading party2', 3, 2, @t6);
+select @t6; #Expect: 1
+
+-- End at the library closing time
+call event_setup('event3', '2023-05-01 14:00:00', '2023-05-01 17:30:00', 5, 'Reading party2', 13, 2, @t7);
+select @t7; #Expect: 1
+
+-- Start at the library start time
+call event_setup('event4', '2023-05-11 9:00:00', '2023-05-11 12:00:00', 5, 'Reading party2', 13, 2, @t8);
+select @t8; #Expect: 1
+
 
 
 

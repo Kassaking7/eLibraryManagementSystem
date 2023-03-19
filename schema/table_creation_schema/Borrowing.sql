2361b418e0283d90371003f4792ae154ed024fef
YuhanZhang11 <83880227+YuhanZhang11@users.noreply.github.com>
2023-03-18 21:30:24 -0400

    Update Borrowing.sql

--- schema/table_creation_schema/Borrowing.sql
+++ schema/table_creation_schema/Borrowing.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Borrowing (
   ISBN              VARCHAR(20) NOT NULL,
   copy_ID           BIGINT NOT NULL,
-  borrowing_ID      BIGINT NOT NULL AUTO_INCREMENT,
+  borrowing_ID      BIGINT NOT NULL,
   borrow_date       DATE NOT NULL,
   return_date       DATE,
   return_deadline   DATE NOT NULL,
   extend_number     INT NOT NULL DEFAULT 0,
   guest_ID          BIGINT NOT NULL,
 
-  PRIMARY KEY (borrowing_ID),
+  PRIMARY KEY (ISBN, copy_ID, borrowing_ID),
 
   FOREIGN KEY (ISBN) REFERENCES Book(ISBN),
   FOREIGN KEY (copy_ID) REFERENCES Copy(copy_ID),

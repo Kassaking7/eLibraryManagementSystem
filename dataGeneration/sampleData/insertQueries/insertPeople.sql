4699964fb36d98cd2b276db2ff7e22ee9e19d587
Zihan Pan <zihanpan8378@gmail.com>
2023-03-21 19:22:14 -0400

    Update

--- dataGeneration/sampleData/insertQueries/insertPeople.sql
+++
@@ -1,8 +0,0 @@
-insert into People (ID, username, password, email_address, is_guest) values (1, 'Alvinia', '0123Kem45mis!@', 'akemmis0@kklib.com', false);
-insert into People (ID, username, password, email_address, is_guest) values (2, 'Sax', 'Mc12!Anell7543ye', 'smcanellye1@kklib.com', false);
-insert into People (ID, username, password, email_address, is_guest) values (3, 'Wally', 'Clu34fwatt!', 'wcluatt2@kklib.com', false);
-insert into People (ID, username, password, email_address, is_guest) values (4, 'Gardner', 'Cheke$#1csd4A', 'gcheke3@kklib.com', false);
-insert into People (ID, username, password, email_address, is_guest) values (5, 'Aeriela', 'Rankin55837!g', 'aranking4@kklib.com', false);
-insert into People (ID, username, password, email_address, is_guest) values (6, 'Frank', 'Sichaopenc!a', 'fsicha5@kklib.com', false);
-insert into People (ID, username, password, email_address, is_guest) values (7, 'Valeria', 'JoevkkrA122!vasevic', 'vjovasevic6@kklib.com', false);
-insert into People (ID, username, password, email_address, is_guest) values (8, 'Bess', 'Mcopencjd!(Reynold', 'bmcreynold@kklib.com', false);

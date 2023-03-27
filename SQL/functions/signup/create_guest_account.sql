80647f16fea5e0625967be1abe80bcddf46c4cc8
Zihan Pan <80815386+zihanpan8378@users.noreply.github.com>
2023-03-27 16:01:03 -0400

    Update create_guest_account.sql

--- SQL/functions/signup/create_guest_account.sql
+++ SQL/functions/signup/create_guest_account.sql
@@ -15,15 +15,23 @@ CREATE PROCEDURE create_guest_account (
   OUT user_ID       BIGINT
 )
 BEGIN
-  SET @default_credit_level = 5;
-  SET @new_user_id = (
-    SELECT MAX(People.ID) + 1
-    FROM People
+  IF (
+    SELECT COUNT(*) 
+    FROM People 
+    WHERE People.username = username
+  ) THEN (
+    SELECT -1 INTO user_ID; -- the given username already exist in the database
+  ) ELSE (
+    SET @default_credit_level = 5;
+    SET @new_user_id = (
+      SELECT MAX(People.ID) + 1
+      FROM People
+    );
+    INSERT INTO People
+      VALUE(@new_user_id, username, password, email_address, TRUE);
+    INSERT INTO Guest
+      VALUE(@new_user_id, FALSE, @default_credit_level, @default_credit_level, DEFAULT);
+    SELECT @new_user_id INTO user_ID;
   );
-  INSERT INTO People
-    VALUE(@new_user_id, username, password, email_address, TRUE);
-  INSERT INTO Guest
-    VALUE(@new_user_id, FALSE, @default_credit_level, @default_credit_level, DEFAULT);
-  SELECT @new_user_id INTO user_ID;
 END //
 DELIMITER ;

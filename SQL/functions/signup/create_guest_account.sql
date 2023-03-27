d02e671c0c41c1f7884fe92d84a340434a6b7256
Kassaking7 <1830604455@qq.com>
2023-03-27 18:13:36 -0400

    rm useless file and update signup function

--- SQL/functions/signup/create_guest_account.sql
+++ SQL/functions/signup/create_guest_account.sql
@@ -6,7 +6,6 @@ DROP PROCEDURE IF EXISTS create_guest_account;
 -- Function: insert a new record into People table and Guest table with the input guest info, where the new guest account is not activated
 -- Input: new guest's information: username, password, and email address
 -- Output: new guest's id
-
 DELIMITER //
 CREATE PROCEDURE create_guest_account (
   IN username       VARCHAR(100),
@@ -19,19 +18,19 @@ BEGIN
     SELECT COUNT(*) 
     FROM People 
     WHERE People.username = username
-  ) THEN (
-    SELECT -1 INTO user_ID; -- the given username already exist in the database
-  ) ELSE (
+  ) THEN 
+    SELECT -1 INTO user_ID; -- the given username already exists in the database
+  ELSE 
     SET @default_credit_level = 5;
     SET @new_user_id = (
       SELECT MAX(People.ID) + 1
       FROM People
     );
     INSERT INTO People
-      VALUE(@new_user_id, username, password, email_address, TRUE);
+      VALUES (@new_user_id, username, password, email_address, TRUE);
     INSERT INTO Guest
-      VALUE(@new_user_id, FALSE, @default_credit_level, @default_credit_level, DEFAULT);
+      VALUES (@new_user_id, FALSE, @default_credit_level, @default_credit_level, DEFAULT);
     SELECT @new_user_id INTO user_ID;
-  );
+  END IF;
 END //
-DELIMITER ;
+DELIMITER ; 
